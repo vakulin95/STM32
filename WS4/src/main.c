737cd75386c690ed12ee419bf39458ae7aa853db
Artyom Vakulin <vakulin95@outlook.com>
2016-12-28 15:00:05 +0300

    [WS4] - PWM control added

--- WS4/src/main.c
+++ WS4/src/main.c
@@ -1,8 +1,3 @@
-//
-// This file is part of the GNU ARM Eclipse distribution.
-// Copyright (c) 2014 Liviu Ionescu.
-//
-
 // ----------------------------------------------------------------------------
 
 #include <stdio.h>
@@ -16,21 +11,30 @@
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
 #define LED_NUM 				(4)
-#define DEFAULT_TIM_PERIOD		(8000.0)
-#define DEFAULT_TIM_PRESCALER	(0)
+
+#define DEFAULT_FREQUENCY		(100.0)
+
+#define DEFAULT_TIM_PRESCALER	(4)
+#define DEFAULT_TIM_PERIOD		(((HSE_VALUE / 2.0) / DEFAULT_TIM_PRESCALER) / DEFAULT_FREQUENCY)
 
 void SystemClockConfig(void);
-void HardPWM(float);
+void HardPWM(int);
 void InitializeLED(GPIO_InitTypeDef*);
+void InitializeButton(void);
+void ChangeBritness(void);
 
 TIM_HandleTypeDef Timer;
 GPIO_InitTypeDef LED[LED_NUM];
 
+float LEDbritness = DEFAULT_TIM_PERIOD / 10;
+
 int main(int argc, char* argv[])
 {
-	SystemClockConfig();
-	HardPWM(20);
+	HardPWM(LEDbritness);
 	InitializeLED(LED);
+	InitializeButton();
+
+	HAL_NVIC_EnableIRQ(EXTI0_IRQn);
 
 	HAL_TIM_Base_Start(&Timer);
 	HAL_TIM_PWM_Start(&Timer, TIM_CHANNEL_1);
@@ -44,26 +48,23 @@ int main(int argc, char* argv[])
 
 void SystemClockConfig(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct;
+	RCC->CFGR &= ~(RCC_CFGR_SW);
+	RCC->CFGR |= RCC_CFGR_SW_HSE;
 
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+	//SystemCoreClockUpdate();
 }
 
-void HardPWM(float div)
+void HardPWM(int imp_len)
 {
-//	RCC->CFGR &= ~(RCC_CFGR_SW);
-//	RCC->CFGR |= RCC_CFGR_SW_HSE;
-
 	TIM_OC_InitTypeDef Hard_PWM_ini1, Hard_PWM_ini2, Hard_PWM_ini3, Hard_PWM_ini4;
+
+	SystemClockConfig();
 
 	__TIM4_CLK_ENABLE();
 
 	Timer.Instance = TIM4;
 
-	Timer.Init.Prescaler = DEFAULT_TIM_PRESCALER;
+	Timer.Init.Prescaler = DEFAULT_TIM_PRESCALER - 1;
 	Timer.Init.CounterMode = TIM_COUNTERMODE_UP;
 	Timer.Init.Period = DEFAULT_TIM_PERIOD;
 
@@ -71,25 +72,25 @@ void HardPWM(float div)
 	HAL_TIM_PWM_Init(&Timer);
 
 	Hard_PWM_ini1.OCMode = TIM_OCMODE_PWM1;
-	Hard_PWM_ini1.Pulse = DEFAULT_TIM_PERIOD / div;
+	Hard_PWM_ini1.Pulse = imp_len;
 	Hard_PWM_ini1.OCPolarity = TIM_OCNPOLARITY_HIGH;
 	Hard_PWM_ini1.OCFastMode = TIM_OCFAST_ENABLE;
 	HAL_TIM_PWM_ConfigChannel(&Timer, &Hard_PWM_ini1, TIM_CHANNEL_1);
 
 	Hard_PWM_ini2.OCMode = TIM_OCMODE_PWM1;
-	Hard_PWM_ini2.Pulse = DEFAULT_TIM_PERIOD / div;
+	Hard_PWM_ini2.Pulse = imp_len;
 	Hard_PWM_ini2.OCPolarity = TIM_OCNPOLARITY_HIGH;
 	Hard_PWM_ini2.OCFastMode = TIM_OCFAST_ENABLE;
 	HAL_TIM_PWM_ConfigChannel(&Timer, &Hard_PWM_ini2, TIM_CHANNEL_2);
 
 	Hard_PWM_ini3.OCMode = TIM_OCMODE_PWM1;
-	Hard_PWM_ini3.Pulse = DEFAULT_TIM_PERIOD / div;
+	Hard_PWM_ini3.Pulse = imp_len;
 	Hard_PWM_ini3.OCPolarity = TIM_OCNPOLARITY_HIGH;
 	Hard_PWM_ini3.OCFastMode = TIM_OCFAST_ENABLE;
 	HAL_TIM_PWM_ConfigChannel(&Timer, &Hard_PWM_ini3, TIM_CHANNEL_3);
 
 	Hard_PWM_ini4.OCMode = TIM_OCMODE_PWM1;
-	Hard_PWM_ini4.Pulse = DEFAULT_TIM_PERIOD / div;
+	Hard_PWM_ini4.Pulse = imp_len;
 	Hard_PWM_ini4.OCPolarity = TIM_OCNPOLARITY_HIGH;
 	Hard_PWM_ini4.OCFastMode = TIM_OCFAST_ENABLE;
 	HAL_TIM_PWM_ConfigChannel(&Timer, &Hard_PWM_ini4, TIM_CHANNEL_4);
@@ -116,6 +117,55 @@ void InitializeLED(GPIO_InitTypeDef *L)
 		HAL_GPIO_Init(GPIOD, &L[i]);
     }
 }
+
+
+void InitializeButton(void)
+{
+
+	GPIO_InitTypeDef UserButton;
+
+	__GPIOA_CLK_ENABLE();
+
+	UserButton.Pin = GPIO_PIN_0;
+	UserButton.Mode = GPIO_MODE_IT_RISING;
+	UserButton.Speed = GPIO_SPEED_FREQ_LOW;
+	UserButton.Pull = GPIO_NOPULL;
+
+	HAL_GPIO_Init(GPIOA, &UserButton);
+
+}
+
+void ChangeBritness(void)
+{
+	float temp = LEDbritness * 0.2;
+	LEDbritness += temp;
+
+	if(LEDbritness < DEFAULT_TIM_PERIOD)
+	{
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_1, LEDbritness);
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_2, LEDbritness);
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_3, LEDbritness);
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_4, LEDbritness);
+	}
+	else
+	{
+		LEDbritness = DEFAULT_TIM_PERIOD / 10.0;
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_1, LEDbritness);
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_2, LEDbritness);
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_3, LEDbritness);
+		__HAL_TIM_SET_COMPARE(&Timer, TIM_CHANNEL_4, LEDbritness);
+	}
+}
+
+//---------------------------------------------------------------
+
+void EXTI0_IRQHandler(void)
+{
+	HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+
+	ChangeBritness();
+}
+
 
 #pragma GCC diagnostic pop
 
