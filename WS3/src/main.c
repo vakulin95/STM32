f4f94016efbc79b85e4cefee92ac9fd8945588df
Artyom Vakulin <vakulin95@outlook.com>
2017-01-03 17:27:10 +0300

    [WS3] - ADC works

--- WS3/src/main.c
+++ WS3/src/main.c
@@ -8,41 +8,29 @@
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
 #pragma GCC diagnostic ignored "-Wreturn-type"
 
-void SystemClockConfig(void);
 void ConfigureADC(void);
 void InitPA5(void);
+void InitPA1(void);
 
-ADC_HandleTypeDef g_AdcHandle;
-GPIO_InitTypeDef PA5ini;
-int g_ADCValue;
+ADC_HandleTypeDef ADCini;
+GPIO_InitTypeDef PA5ini, PA1ini;
+int ADCval;
 
 int main(int argc, char* argv[])
 {
-	//SystemClockConfig();
-	//InitPA5();
-
 	ConfigureADC();
 
 	while(1)
 	{
-		HAL_ADC_Start(&g_AdcHandle);
-		while(!(ADC1->SR & ADC_SR_EOC)) {};
-		//g_ADCValue = HAL_ADC_GetValue(&g_AdcHandle);
-		g_ADCValue = ADC1->DR;
+		HAL_ADC_Start(&ADCini);
+		while(!(ADC1->SR & ADC_SR_EOC));
+		ADCval = ADC1->DR;
 	}
-}
-
-void SystemClockConfig(void)
-{
-	RCC->CFGR &= ~(RCC_CFGR_SW);
-	RCC->CFGR |= RCC_CFGR_SW_HSE;
-
-	//SystemCoreClockUpdate();
 }
 
 void InitPA5(void)
 {
-	__GPIOA_CLK_ENABLE();
+	//__GPIOA_CLK_ENABLE();
 
 	PA5ini.Pin = GPIO_PIN_5;
 	PA5ini.Mode = GPIO_MODE_OUTPUT_PP;
@@ -51,40 +39,47 @@ void InitPA5(void)
 
 	HAL_GPIO_Init(GPIOA, &PA5ini);
 
-	GPIO
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
+}
 
-	//HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_SET);
-	//HAL_GPIO_TogglePin(GPIOA, PA5ini.Pin);
+void InitPA1(void)
+{
+	//__GPIOA_CLK_ENABLE();
+
+	PA1ini.Pin = GPIO_PIN_1;
+	PA1ini.Mode = GPIO_MODE_ANALOG;
+	PA1ini.Pull = GPIO_PULLUP;
+
+	HAL_GPIO_Init(GPIOA, &PA1ini);
 }
 
 void ConfigureADC(void)
 {
-    GPIO_InitTypeDef gpioInit;
-
-    InitPA5();
+//    GPIO_InitTypeDef gpioInit;
 
     __GPIOA_CLK_ENABLE();
     __ADC1_CLK_ENABLE();
 
-    gpioInit.Pin = GPIO_PIN_1;
-    gpioInit.Mode = GPIO_MODE_ANALOG;
-    gpioInit.Pull = GPIO_PULLUP;
+    InitPA5();
+    InitPA1();
 
-    HAL_GPIO_Init(GPIOA, &gpioInit);
+//    gpioInit.Pin = GPIO_PIN_1;
+//    gpioInit.Mode = GPIO_MODE_ANALOG;
+//    gpioInit.Pull = GPIO_PULLUP;
+//
+//    HAL_GPIO_Init(GPIOA, &gpioInit);
 
-    g_AdcHandle.Instance = ADC1;
-    g_AdcHandle.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
-    //ADC driver configuration
-    g_AdcHandle.Init.Resolution = ADC_RESOLUTION_8B;
-    g_AdcHandle.Init.ScanConvMode = DISABLE;
-    g_AdcHandle.Init.ContinuousConvMode = ENABLE;
-    g_AdcHandle.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    g_AdcHandle.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
-    g_AdcHandle.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-    g_AdcHandle.Init.NbrOfConversion = 1;
+    ADCini.Instance = ADC1;
+    ADCini.Init.ClockPrescaler = ADC_CLOCKPRESCALER_PCLK_DIV2;
+    ADCini.Init.Resolution = ADC_RESOLUTION_8B;
+    ADCini.Init.ScanConvMode = DISABLE;
+    ADCini.Init.ContinuousConvMode = ENABLE;
+    ADCini.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    ADCini.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_CC1;
+    ADCini.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    ADCini.Init.NbrOfConversion = 1;
 
-
-    HAL_ADC_Init(&g_AdcHandle);
+    HAL_ADC_Init(&ADCini);
 
     ADC_ChannelConfTypeDef adcChannel;
 
@@ -93,7 +88,7 @@ void ConfigureADC(void)
     adcChannel.SamplingTime = ADC_SAMPLETIME_144CYCLES;
     adcChannel.Offset = 0;
 
-    HAL_ADC_ConfigChannel(&g_AdcHandle, &adcChannel);
+    HAL_ADC_ConfigChannel(&ADCini, &adcChannel);
 }
 
 #pragma GCC diagnostic pop
