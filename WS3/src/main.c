0dbf91c80cf678c45e6e08c92cb698899ca1401a
Artyom Vakulin <vakulin95@outlook.com>
2017-01-03 18:40:34 +0300

    [WS3] - Added indication on LEDs

--- WS3/src/main.c
+++ WS3/src/main.c
@@ -14,12 +14,13 @@ void InitPA5(void);
 void InitPA1(void);
 void ConfigureADC(void);
 void InitLED(GPIO_InitTypeDef *L);
+int GetADC(int ADCval);
+void SwitchLED(GPIO_InitTypeDef *L, int N);
 
 ADC_HandleTypeDef ADCini;
 ADC_ChannelConfTypeDef ADCchan;
 GPIO_InitTypeDef PA5ini, PA1ini;
 GPIO_InitTypeDef LED[LED_NUM];
-int ADCval;
 
 int main(int argc, char* argv[])
 {
@@ -30,7 +31,7 @@ int main(int argc, char* argv[])
 	{
 		HAL_ADC_Start(&ADCini);
 		while(!(ADC1->SR & ADC_SR_EOC));
-		ADCval = ADC1->DR;
+		SwitchLED(LED, GetADC(ADC1->DR));
 	}
 }
 
@@ -101,8 +102,55 @@ void InitLED(GPIO_InitTypeDef *L)
     	L[i].Pull = GPIO_NOPULL;
 
 		HAL_GPIO_Init(GPIOD, &L[i]);
-		HAL_GPIO_WritePin(GPIOD, L[i].Pin, GPIO_PIN_SET);
     }
+}
+
+int GetADC(int ADCval)
+{
+	int Y = 0;
+
+	if(!ADCval)
+	{
+		goto ret;
+	}
+	else if(ADCval > 0 && ADCval < 64)
+	{
+		Y = 1;
+		goto ret;
+	}
+	else if(ADCval >= 64 && ADCval < 128)
+	{
+		Y = 2;
+		goto ret;
+	}
+	else if(ADCval >= 128 && ADCval < 192)
+	{
+		Y = 3;
+		goto ret;
+	}
+	else if(ADCval >= 192 && ADCval < 256)
+	{
+		Y = 4;
+		goto ret;
+	}
+
+ret:
+	return Y;
+}
+
+void SwitchLED(GPIO_InitTypeDef *L, int N)
+{
+	int i;
+
+	for(i = 0; i < LED_NUM; i++)
+	{
+		HAL_GPIO_WritePin(GPIOD, L[i].Pin, GPIO_PIN_RESET);
+	}
+
+	for(i = 0; i < N; i++)
+	{
+		HAL_GPIO_WritePin(GPIOD, L[i].Pin, GPIO_PIN_SET);
+	}
 }
 
 #pragma GCC diagnostic pop
